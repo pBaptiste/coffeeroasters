30bd29289cc9711f1695a7f3930f5d064916e57b
Pierre Baptiste <pierrebaptiste@Pierres-MacBook-Pro.local>
2023-11-08 10:13:05 -0500

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth'>
       <body className={`${barlow.variable} ${fraunces.variable} relavite flex flex-col items-center bg-lightCream`}>
         <Navbar />
         {children}
